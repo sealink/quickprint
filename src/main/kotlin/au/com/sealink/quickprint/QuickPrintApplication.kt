728e6e64a8a4ddce5851b6cd2bcf8a15a33b8b5a
John Mortlock <john.mortlock@gmail.com>
2018-09-03 19:45:16 +0930

    Work as a WAR

--- src/main/kotlin/au/com/sealink/quickprint/QuickPrintApplication.kt
+++ src/main/kotlin/au/com/sealink/quickprint/QuickPrintApplication.kt
@@ -2,9 +2,18 @@ package au.com.sealink.quickprint
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer
+import org.springframework.boot.builder.SpringApplicationBuilder
+
+
 
 @SpringBootApplication
-class QuickPrintApplication
+class QuickPrintApplication : SpringBootServletInitializer() {
+    override fun configure(builder: SpringApplicationBuilder): SpringApplicationBuilder {
+        return builder.sources(QuickPrintApplication::class.java)
+    }
+}
+
 fun main(args: Array<String>) {
     runApplication<QuickPrintApplication>(*args)
 }
