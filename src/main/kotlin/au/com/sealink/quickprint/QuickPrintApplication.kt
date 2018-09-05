9785c1e298feeaeb59cd3fe5012576553e40bd9a
John Mortlock <john.mortlock@gmail.com>
2018-09-05 15:38:19 +0930

    Add support to run as a docker container

--- src/main/kotlin/au/com/sealink/quickprint/QuickPrintApplication.kt
+++ src/main/kotlin/au/com/sealink/quickprint/QuickPrintApplication.kt
@@ -5,8 +5,6 @@ import org.springframework.boot.runApplication
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer
 import org.springframework.boot.builder.SpringApplicationBuilder
 
-
-
 @SpringBootApplication
 class QuickPrintApplication : SpringBootServletInitializer() {
     override fun configure(builder: SpringApplicationBuilder): SpringApplicationBuilder {
