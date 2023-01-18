ea82772309281d9dbb9677cfacc4391990a56ffb
Stefan Cooper <stefan.cooper@sealink.com.au>
2023-01-18 15:13:02 +1030

    TT-11531 Updated PageFormat constructor
    Removed optional margin param, constructor now
    only takes the margin_x and margin_y params

--- src/main/kotlin/au/com/sealink/quickprint/requests/PageFormat.kt
+++ src/main/kotlin/au/com/sealink/quickprint/requests/PageFormat.kt
@@ -1,4 +1,4 @@
 package au.com.sealink.quickprint.requests
 
 
-data class PageFormat(val width: Double, val height: Double, val margin: Double?, val margin_x: Double?, val margin_y: Double?)
+data class PageFormat(val width: Double, val height: Double, val margin_x: Double?, val margin_y: Double?)
