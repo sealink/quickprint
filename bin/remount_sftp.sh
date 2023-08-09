8800871bd1bda944e6404e94bb1fa2102e327953
DamonShi1024 <damon.shi@kelsian.com>
2023-08-09 14:35:57 +0930

    [TF-695] add remount script

---
+++ bin/remount_sftp.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+# Define your mount point
+MOUNT_POINT="/mnt/nfs"
+
+# Check if the mount point is already mounted
+if mountpoint -q "$MOUNT_POINT"; then
+    echo "Mount point $MOUNT_POINT is already mounted."
+else
+    echo "Mount point $MOUNT_POINT is not mounted. Mounting now..."
+    
+    SFTP_PASSWORD=$(sudo /opt/elasticbeanstalk/bin/get-config environment -k SFTP_PASSWORD)
+    echo "$SFTP_PASSWORD" > /tmp/credentials
+    sudo sshfs -o allow_other,StrictHostKeyChecking=no,password_stdin sftp-user@172.18.0.12:/ /mnt/nfs < /tmp/credentials
+    rm /tmp/credentials
+    
+    echo "Mounting complete."
+fi
