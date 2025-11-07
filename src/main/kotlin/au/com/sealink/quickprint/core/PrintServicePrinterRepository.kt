50070a38cd6167d9586ae1c7ac5a6d30f5109843
James Brooks <james@gooddogdesign.com>
2025-11-07 14:31:03 +1030

    Add additional logging around known spot of errors to try and gather more info

--- src/main/kotlin/au/com/sealink/quickprint/core/PrintServicePrinterRepository.kt
+++ src/main/kotlin/au/com/sealink/quickprint/core/PrintServicePrinterRepository.kt
@@ -2,15 +2,20 @@ package au.com.sealink.quickprint.core
 
 import au.com.sealink.printing.ticketprinter.PrintServiceLocator
 import org.springframework.stereotype.Repository
+import java.time.LocalDateTime
 import javax.print.PrintService
 
 @Repository
 class PrintServicePrinterRepository : PrinterRepository {
     override fun requestPrinter(name: String): Printer {
+        System.err.println("[${LocalDateTime.now()}] PrinterRepository REQUEST: printer='$name'")
         return TicketPrinter(name)
     }
 
     override fun findAll(): Iterable<PrintService> {
-        return PrintServiceLocator().all.toList()
+        System.err.println("[${LocalDateTime.now()}] PrinterRepository ENUM START")
+        val services = PrintServiceLocator().all.toList()
+        System.err.println("[${LocalDateTime.now()}] PrinterRepository ENUM FOUND: count=${services.size}, names=${services.joinToString(",") { it.name }}")
+        return services
     }
 }
--- src/main/kotlin/au/com/sealink/quickprint/core/TicketPrinter.kt
+++ src/main/kotlin/au/com/sealink/quickprint/core/TicketPrinter.kt
@@ -3,19 +3,31 @@ package au.com.sealink.quickprint.core
 import au.com.sealink.printing.ticketprinter.Ticket
 import au.com.sealink.printing.ticketprinter.TicketPageSettings
 import au.com.sealink.printing.ticketprinter.TicketPrinter
+import java.time.LocalDateTime
 
 class TicketPrinter(private var name: String) : Printer {
     private val mPrinter : TicketPrinter = TicketPrinter()
 
     init {
+        System.err.println("[${LocalDateTime.now()}] TicketPrinter INIT: printer='$name'")
         mPrinter.setPrinter(this.name)
+        System.err.println("[${LocalDateTime.now()}] TicketPrinter INIT SUCCESS: printer='$name'")
     }
 
     override fun setTicketPageSettings(settings: TicketPageSettings) {
+        System.err.println("[${LocalDateTime.now()}] TicketPrinter SET_SETTINGS: printer='$name'")
         mPrinter.setTicketPageSettings(settings)
     }
 
     override fun printTickets(tickets: List<Ticket>) {
-        mPrinter.printTickets(tickets)
+        try {
+            System.err.println("[${LocalDateTime.now()}] TicketPrinter PRINT START: printer='$name', tickets=${tickets.size}, thread=${Thread.currentThread().name}")
+            mPrinter.printTickets(tickets)
+            System.err.println("[${LocalDateTime.now()}] TicketPrinter PRINT SUCCESS: printer='$name'")
+        } catch (e: Exception) {
+            System.err.println("[${LocalDateTime.now()}] TicketPrinter PRINT FAILED: printer='$name', error=${e.javaClass.name}: ${e.message}")
+            e.printStackTrace()
+            throw e
+        }
     }
 }
